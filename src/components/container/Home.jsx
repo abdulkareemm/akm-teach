9dc25a66c1912cba907a40994fa95b203baeabc2
abdulkareemm <eng.kreem.96@gmail.com>
2023-01-29 04:44:18 -0800

    home component

--- src/components/container/Home.jsx
+++ src/components/container/Home.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import hero from "../../assets/hero.png";
+import { logos } from "../../Data";
 
 const Home = () => {
   return (
-    <div className='section' id="home">Home</div>
-  )
-}
+    <div className="section" id="home">
+      <div className="md:flex items-center justify-center">
+        <div>
+          <div className="font-bold text-xs text-Teal mb-4">
+            your e-learning partner
+          </div>
+          <div className="sm:text-[2.5rem] text-[1.825rem] font-bold">
+            This is <br /> the new way <br /> to learn online
+          </div>
+          <p className="text-sm leading-7 text-gray max-w-sm">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Tristique magna sit amet purus gravida quis blandit turpis cursus.
+            Amet consectetur adipiscing elit ut aliquam purus sit amet luctus.
+          </p>
+          <div className="mt-6">
+            <button className="px-6 py-3 font-bold text-white bg-Teal rounded-lg mr-4 text-sm ">
+              Get Started
+            </button>
+            <button className="px-6 py-3 font-bold border border-gray rounded-lg text-sm">
+              Discover
+            </button>
+          </div>
+        </div>
+        <div className="md:w-[60%]">
+          <img src={hero} alt="" />
+        </div>
+      </div>
+      <div className="">
+        <p className="text-center text-xl">
+          We collaborate with{" "}
+          <span className="text-Teal">100+ leading universites ans companies</span>
+        </p>
+        <div className="flex items-center justify-center flex-wrap gap-8 p-2">
+          {logos.map((logo,idx)=>(
+            <div className="w-28" key={idx}>
+              <img src={logo} alt="" className="w-full object-cover"/>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
